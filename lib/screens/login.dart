da5c8eaf84211466d6e35522e8808e7b6f683b36
kapiljhajhria <Aerospace44>
2020-05-11 09:37:26 +0530

    rough login UI completed

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/material.dart';
 
 class LogInScreen extends StatefulWidget {
@@ -21,16 +19,15 @@ class _LogInScreenState extends State<LogInScreen> {
                 border: OutlineInputBorder(),
                 hintText: fieldName,
                 contentPadding:
-                EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                    EdgeInsets.symmetric(horizontal: 20, vertical: 10),
                 hintStyle:
-                TextStyle(fontWeight: FontWeight.w600, fontSize: 18)),
+                    TextStyle(fontWeight: FontWeight.w600, fontSize: 18)),
           ),
         ),
         padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
       ),
     );
   }
-
 
   @override
   void initState() {
@@ -39,122 +36,103 @@ class _LogInScreenState extends State<LogInScreen> {
     super.initState();
   }
 
-
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.fromLTRB(
-          0,
-          MediaQuery.of(context).size.height * .04,
-          0,
-          MediaQuery.of(context).size.height * .1),
-      child: Scaffold(
-        body: Stack(
-          children: <Widget>[
-            ColorFiltered(
-              colorFilter: ColorFilter.mode(
-                  Color.fromRGBO(255, 122, 122, 1.0), BlendMode.color),
-              child: Container(
-                width: double.infinity,
-                height: double.infinity,
-                color: Colors.green,
-              ),
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          ColorFiltered(
+            colorFilter: ColorFilter.mode(
+                Color.fromRGBO(255, 122, 122, 1.0), BlendMode.color),
+            child: Container(
+              width: double.infinity,
+              height: double.infinity,
+              color: Colors.green,
             ),
-            Container(
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.all(Radius.circular(10))),
-                margin: EdgeInsets.fromLTRB(
-                    MediaQuery.of(context).size.width * .08,
-                    MediaQuery.of(context).size.height * .04,
-                    MediaQuery.of(context).size.width * .08,
-                    0),
-                child: Column(
-                  children: <Widget>[
-                    formInputTextBox("First Name"),
-                    formInputTextBox("Last Name"),
-                    formInputTextBox("Email Address"),
-                    formInputTextBox("Password"),
-                    GestureDetector(
-                      onTap: () {
-                        myScrollController.animateTo(
-                            -MediaQuery.of(context).size.height,
-                            duration: Duration(milliseconds: 500),
-                            curve: Curves.linear);
-                        setState(() {});
-                      },
-                      child: Container(
-                        decoration: BoxDecoration(
-                          borderRadius:
-                          BorderRadius.all(Radius.circular(7)),
-                          color: Color.fromRGBO(56, 204, 140, 1.0),
-                          boxShadow: [
-                            BoxShadow(
-                                color: Colors.green,
-                                offset: Offset(0, 4.0),
-                                spreadRadius: 0.0)
-                          ],
-                        ),
-                        margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
-                        height: MediaQuery
-                            .of(context)
-                            .size
-                            .height * 0.09,
-                        width: MediaQuery
-                            .of(context)
-                            .size
-                            .width * 0.75,
-                        child: Center(
-                          child: Text(
-                            "CLAIM YOUR FREE TRIAL",
-                            style: TextStyle(
-                                fontSize: 19,
-                                color: Colors.white,
-                                fontWeight: FontWeight.w600,
-                                wordSpacing: 2),
-                          ),
+          ),
+          Container(
+//              padding: EdgeInsets.fromLTRB(
+//                  0,
+//                  MediaQuery.of(context).size.height * .04,
+//                  0,
+//                  MediaQuery.of(context).size.height * .1),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(10))),
+              margin: EdgeInsets.fromLTRB(
+                  MediaQuery.of(context).size.width * .08,
+                  MediaQuery.of(context).size.height * .1,
+                  MediaQuery.of(context).size.width * .08,
+                  MediaQuery.of(context).size.height * .1),
+              child: Column(
+                children: <Widget>[
+                  formInputTextBox("Email"),
+                  formInputTextBox("Password"),
+                  GestureDetector(
+                    onTap: () {
+
+                      setState(() {});
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(7)),
+                        color: Color.fromRGBO(56, 204, 140, 1.0),
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.green,
+                              offset: Offset(0, 4.0),
+                              spreadRadius: 0.0)
+                        ],
+                      ),
+                      margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                      height: MediaQuery.of(context).size.height * 0.09,
+                      width: MediaQuery.of(context).size.width * 0.75,
+                      child: Center(
+                        child: Text(
+                          "LogIn",
+                          style: TextStyle(
+                              fontSize: 19,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w600,
+                              wordSpacing: 2),
                         ),
                       ),
                     ),
-                    Expanded(
-                      flex: 1,
-                      child: Container(
-                          width: MediaQuery.of(context).size.width * 0.60,
-                          margin: EdgeInsets.only(top: 10),
-                          alignment: Alignment(1, -0.4),
-                          child: RichText(
-                            textAlign: TextAlign.center,
-                            text: TextSpan(
-                              style: TextStyle(
-                                color: Colors.white,
-                              ),
-                              children: <TextSpan>[
-                                TextSpan(
-                                    text:
-                                    'By clicking the button, you are agreeing to our  ',
-                                    style: TextStyle(
-                                        fontSize: 14,
-                                        color: Color.fromRGBO(
-                                            185, 182, 211, 1.0),
-                                        fontWeight: FontWeight.w400)),
-                                TextSpan(
-                                    text: "Terms and Services",
-                                    style: TextStyle(
-                                        fontSize: 14,
-                                        color: Colors.red,
-                                        fontWeight: FontWeight.w600)),
-                              ],
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                        width: MediaQuery.of(context).size.width * 0.60,
+                        margin: EdgeInsets.only(top: 10),
+                        alignment: Alignment(1, -0.4),
+                        child: RichText(
+                          textAlign: TextAlign.center,
+                          text: TextSpan(
+                            style: TextStyle(
+                              color: Colors.white,
                             ),
-                          )),
-                    ),
-                  ],
-                )),
-          ],
-        ),
+                            children: <TextSpan>[
+                              TextSpan(
+                                  text:
+                                      'By clicking the button, you are agreeing to our  ',
+                                  style: TextStyle(
+                                      fontSize: 14,
+                                      color: Color.fromRGBO(185, 182, 211, 1.0),
+                                      fontWeight: FontWeight.w400)),
+                              TextSpan(
+                                  text: "Terms and Services",
+                                  style: TextStyle(
+                                      fontSize: 14,
+                                      color: Colors.red,
+                                      fontWeight: FontWeight.w600)),
+                            ],
+                          ),
+                        )),
+                  ),
+                ],
+              )),
+        ],
       ),
-//                  color: Color.fromRGBO(56, 204, 140, 1.0),
-      width: MediaQuery.of(context).size.width,
-      height: MediaQuery.of(context).size.height,
     );
   }
 }
