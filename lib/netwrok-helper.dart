889abb052d8e039e6c50ffed7f0539497ceff8c7
kapiljhajhria <Aerospace44>
2020-05-12 10:38:36 +0530

    getting cookie using a function now

--- lib/netwrok-helper.dart
+++ lib/netwrok-helper.dart
@@ -6,8 +6,7 @@ class Session {
   Map<String, String> headers = {
     'Content-Type': 'application/json',
   };
-  String koki;
-  String kokiSig;
+  String cookie = "";
 
   Future get(String url) async {
     print("printing headers of get request");
@@ -17,23 +16,29 @@ class Session {
     return json.decode(response.body);
   }
 
+  String getCookie(String cookieString) {
+    String cookie = "";
+    List cookiesList = cookieString.split(";");
+//    print(cookiesList);
+    cookie = cookie + cookiesList[0] + ";" + cookiesList[3].split(",")[1] + ";";
+    print("cookie is: " + cookie);
+    return cookie;
+  }
+
   Future<Map> post(String url, dynamic data) async {
     http.Response response =
         await http.post(url, body: jsonEncode(data), headers: headers);
     print("let see headers");
-    String cookie = response.headers["set-cookie"];
-    cookie = cookie.replaceAll(",", " ");
-    print(cookie.split(";")[0]);
-    print(cookie.split(";")[6]);
-    print(cookie);
+//    String cookie = response.headers["set-cookie"];
+//    print(cookie);
     updateCookie(response);
     return json.decode(response.body);
   }
 
   void updateCookie(http.Response response) {
     String rawCookie = response.headers['set-cookie'];
-    headers['cookie'] =
-    "forms-login=eyJ1c2VySWQiOiI1ZWIzYmNjMTE3ZmVmNTJlYWNiZTA2YzkifQ==;forms-login.sig=ySObHjT5HQphV0p_VHHHZk1Ue9I;";
-
+    if (rawCookie != null) {
+      headers['cookie'] = getCookie(rawCookie);
+    }
   }
 }
